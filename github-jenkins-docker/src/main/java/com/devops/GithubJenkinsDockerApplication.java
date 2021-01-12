87f10c6d221d4b1c15593eaab0cec1aefb7a2f26
invin_ng <invin_ng@RS-NB21.RINGUS.LOCAL>
2021-01-12 17:13:01 +0800

    hello world

---
+++ github-jenkins-docker/src/main/java/com/devops/GithubJenkinsDockerApplication.java
@@ -0,0 +1,13 @@
+package com.devops;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubJenkinsDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubJenkinsDockerApplication.class, args);
+	}
+
+}
---
+++ github-jenkins-docker/src/main/java/com/devops/Hello.java
@@ -0,0 +1,9 @@
+package com.devops;
+
+public class Hello {
+	void main() {
+		for(int i=1; i<=10; i++) {
+			System.out.println("Hello World!");
+		}
+	}
+}
